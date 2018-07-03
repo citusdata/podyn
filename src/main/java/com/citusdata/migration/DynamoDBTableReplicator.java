2a0572d749d6b990a82879da8aac0f0af9c80bbd
Jared Leonard <jaredmyronarthurleonard@gmail.com>
2018-07-03 19:52:00 +0200

    null check on columnValue

--- src/main/java/com/citusdata/migration/DynamoDBTableReplicator.java
+++ src/main/java/com/citusdata/migration/DynamoDBTableReplicator.java
@@ -562,10 +562,9 @@ public class DynamoDBTableReplicator {
             AttributeValue typedValue = entry.getValue();
             TableColumnValue columnValue = columnValueFromDynamoValue(typedValue);
 
-            if (columnValue.type == column.type) {
+            if (columnValue != null && columnValue.type == column.type) {
                 row.setValue(columnName, columnValue);
             } else {
-
                 row.setValue(columnName + "_" + columnValue.type, columnValue);
             }
         }
