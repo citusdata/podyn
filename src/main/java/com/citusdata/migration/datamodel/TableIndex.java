f3bf5106693fffe1c0fa82fff24f94730fd43544
E.J. Finneran <ej.finneran@gmail.com>
2021-06-25 09:25:44 -0700

    Join table and index name on underscore instead of dash

--- src/main/java/com/citusdata/migration/datamodel/TableIndex.java
+++ src/main/java/com/citusdata/migration/datamodel/TableIndex.java
@@ -11,7 +11,7 @@ public class TableIndex {
 
 	public TableIndex(String tableName, String name, List<String> columnNames) {
 		this.tableName = tableName;
-		this.name = String.join("-", this.tableName, name);
+		this.name = String.join("_", this.tableName, name);
 		this.columnNames = columnNames;
 	}
 	
